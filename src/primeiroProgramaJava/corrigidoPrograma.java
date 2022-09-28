d4c9cf368dd8c90a20809713f9f81602a9e4982e
RafaelDeVasconcelos <rafaelvasconcelos321@gmail.com>
2022-09-28 20:38:17 -0300

    Primeiro commit

---
+++ src/primeiroProgramaJava/corrigidoPrograma.java
@@ -0,0 +1,13 @@
+package primeiroProgramaJava;
+
+public class corrigidoPrograma {
+
+	public static void main(String[] args) {
+			
+			Gato gatos = new Gato("Brayan", "preto", 6);
+			System.out.println(gatos);
+
+		}
+	}
+
+
